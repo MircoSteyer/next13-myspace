b0cb2537bf91ac51d92ca606b7db2641c330a013
Mirco Steyer <m.steyer@finatix.de>
2023-05-30 13:37:58 +0200

    add database connection

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
 const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Github({
       clientId: process.env.GITHUB_ID!,
